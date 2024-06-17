27ee74e57d2f57e11f9f63f8af64b2faba914783
UADER_IS2_Cardinaux <127860186+NicolasCardinaux@users.noreply.github.com>
2024-06-17 13:42:36 -0300

    Delete Apis directory

--- Apis/Proyecto_Api/api_jugadoresequipos/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from api_jugadoresequipos.models import Jugador, Equipo
-
-# Registra tus modelos aquí.
-
-admin.site.register(Jugador)
-admin.site.register(Equipo)
